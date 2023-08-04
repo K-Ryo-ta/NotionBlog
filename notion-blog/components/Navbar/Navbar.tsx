802a11c8f15849db757632f2544821d61e8d3a49
K-Ryo-ta <studyprograming51@gmail.com>
2023-08-04 19:10:26 +0900

    add:ナビゲーションバーの実装。ナビゲーションバーのスタイルを整えた。

---
+++ notion-blog/components/Navbar/Navbar.tsx
@@ -0,0 +1,31 @@
+import React from "react";
+import Link from "next/link";
+const Navbar = () => {
+    return (
+        <nav className="container mx-auto lg:px-2 px-5 lg:w-2/5">
+            <div className="container flex items-center justify-between mx-auto">
+                <Link href="/" className="text-2xl font-semibold">
+                    Ryota
+                </Link>
+                <div>
+                    <ul className="flex items-center text-sm py-4">
+                        <li>
+                            <Link href="/" className="block px-4 py-2 hover:text-sky-900 transition-all duration-300 font-semibold">Home</Link>
+                        </li>
+                        <li>
+                            <Link href="https://twitter.com/KR_programing" className="block px-4 py-2 hover:text-sky-900 transition-all duration-300 font-semibold">Twitter</Link>
+                        </li>
+                        <li>
+                            <Link href="https://qiita.com/Ryota_programing" className="block px-4 py-2 hover:text-sky-900 transition-all duration-300 font-semibold">Qiita</Link>
+                        </li>
+                        <li>
+                            <Link href="https://github.com/K-Ryo-ta" className="block px-4 py-2 hover:text-sky-900 transition-all duration-300 font-semibold">GitHub</Link>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    );
+};
+
+export default Navbar;
