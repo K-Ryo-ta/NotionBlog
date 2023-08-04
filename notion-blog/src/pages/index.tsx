559df6b85a018a0093b73840a40deb15a38f7d34
K-Ryo-ta <studyprograming51@gmail.com>
2023-08-04 11:34:49 +0900

    add:一つの要素をweb上に表示できた。

--- notion-blog/src/pages/index.tsx
+++ notion-blog/src/pages/index.tsx
@@ -1,4 +1,6 @@
 import { getAllPosts } from "../../lib/notionAPI";
+import Head from "next/head";
+import SinglePost from "../../components/Post/SinglePost"
 export const getStaticProps = async () => {
   const allPosts = await getAllPosts();
 
@@ -13,7 +15,27 @@ export const getStaticProps = async () => {
 export default function Home({ allPosts }: any) {
   console.log(allPosts);
   return (
-    <div>
+    <div className="container h-full mx-auto font-mono">
+      <Head>
+        <title>Notion-Blog</title>
+      </Head>
+
+      <main className="container w-full mt-16">
+        <h1 className="text-5xl font-medium text-center mg-16">Notion Blog🚀</h1>
+      </main>
+      {
+        allPosts.map((post: any) => (
+          <div className="mx-4">
+            <SinglePost
+              title={post.title}
+              desciption={post.desciption}
+              date={post.date}
+              tag={post.tags}
+              slug={post.slug}
+            />
+          </div>
+        ))
+      }
     </div>
   )
 }
