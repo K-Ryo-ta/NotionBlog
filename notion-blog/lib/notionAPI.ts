4a9ff8953bb79273b17ce7434eb7b60123f2cea4
K-Ryo-ta <studyprograming51@gmail.com>
2023-08-05 21:38:51 +0900

    add:レイアウト整理・動的ルーティングの設定・マークダウンで取得・シンタックスハイライト機能追加。

--- notion-blog/lib/notionAPI.ts
+++ notion-blog/lib/notionAPI.ts
@@ -1,8 +1,11 @@
 import { Client } from "@notionhq/client";
+import {NotionToMarkdown} from "notion-to-md";
 
 const notion = new Client({
     auth: process.env.NOTION_TOKEN,
   })
+
+  const n2m = new NotionToMarkdown({ notionClient: notion });
 
   export  const getAllPosts = async () =>{
     const posts = await notion.databases.query({
@@ -33,4 +36,27 @@ const notion = new Client({
       slug: post.properties.Slug.rich_text[0].plain_text,
       tags: getTags(post.properties.タグ.multi_select),
     };
+  };
+
+  export const getSinglePost = async (slug:string) =>{
+    const response = await notion.databases.query({
+      database_id: process.env.NOTION_DATABASE_ID!,
+      filter: {
+        property:"Slug",
+        formula:{
+          string:{
+            equals: slug,
+          }
+        }
+      },
+    });
+    const page = response.results[0];
+    const metadata = getPageMetaData(page);
+    const mdblocks = await n2m.pageToMarkdown(page.id);
+    const mdString = n2m.toMarkdownString(mdblocks);
+    console.log(mdString);
+    return{
+      metadata,
+      markdown:mdString,
+    }
   };
---
+++ notion-blog/src/pages/post/[slug].tsx
@@ -0,0 +1,78 @@
+import React from "react";
+import { getSinglePost, getAllPosts } from "../../../lib/notionAPI";
+import { ReactMarkdown } from "react-markdown/lib/react-markdown";
+import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
+import { prism } from 'react-syntax-highlighter/dist/cjs/styles/prism';
+import Link from "next/link";
+
+export const getStaticPaths = async () => {
+    const allPosts = await getAllPosts();
+    const paths = allPosts.map((slug) => ({ params: { slug: slug.slug } }))
+    return {
+        paths,
+        fallback: "blocking",
+    }
+}
+
+export const getStaticProps = async ({ params }: any) => {
+    const post = await getSinglePost(params.slug);
+    return {
+        props: {
+            post: post,
+        },
+        revalidate: 60,
+    }
+}
+
+const Post = ({ post }: any) => {
+    return (
+        <section className="container lg:px-2 px-5 h-screen lg:w-2/5 mx-auto mt-20">
+            <h2 className="w-full text-2xl font-semibold">
+                {post.metadata.title}
+            </h2>
+            <div className="border-b-2 w-full mt-1 border-sky-900">
+            </div>
+            <span className="text-gray-500">
+                Posted dated at {post.metadata.date}
+            </span>
+            <br />
+            {post.metadata.tags.map((tag: string) => (
+                <p className="text-white bg-sky-900 rounded-xl font-semibold mt-2 px-2 inline-block mr-2">
+                    {tag}
+                </p>
+            ))}
+
+            <div className="mt-10 font-semibold">
+                <ReactMarkdown components={{
+                    code({ node, inline, className, children, ...props }) {
+                        const match = /language-(\w+)/.exec(className || '')
+                        return !inline && match ? (
+                            <SyntaxHighlighter
+                                {...props}
+                                children={String(children).replace(/\n$/, '')}
+                                style={prism}
+                                language={match[1]}
+                                PreTag="div"
+                            />
+                        ) : (
+                            <code {...props} className={className}>
+                                {children}
+                            </code>
+                        )
+                    }
+                }}>
+                    {post.markdown.parent}
+                </ReactMarkdown>
+            </div>
+
+            <Link href="/">
+                <span className="block px-2 py-2 hover:text-orange-600 transition-all duration-300 font-semibold bg-gray-300 rounded-md shadow-2xl hover:shadow-none hover:translate-y-0.5 mx-1 inline-block">
+                    ホームに戻る
+                </span>
+            </Link>
+
+        </section>
+    );
+};
+
+export default Post;
